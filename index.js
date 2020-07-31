239ea4fe452956b3772d42be6e5433b1ed4b0455
Nitin Mukesh <nitinmukes@gmail.com>
2020-08-01 00:15:53 +0800

    Added addRole, addEmployee and viewDepartments logic

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const chalk = require('chalk');
 const clear = require('clear');
+const chalk = require('chalk');
 const figlet = require('figlet');
-const { questions, continueQuestion } = require('./lib/questions');
-const { addDepartment, addEmployee, addRole, fetchData, fetchDepartment } = require('./lib/queryEmployeeDB');
+const cTable = require('console.table');
 const inquirer = require('inquirer');
+const { questions, continueQuestion } = require('./lib/questions');
+const { addDepartment, addEmployee, addRole, fetchData, fetchEmployeeByName } = require('./lib/queryEmployeeDB');
+
 
 const initializeDisplay = () => {
     clear();
@@ -14,30 +16,40 @@ const initializeDisplay = () => {
     );
 }
 
-const aksQuestions = () => {
-    inquirer.prompt(questions).then(async answers => {
-        switch (answers.action) {
-            case 'Add Department':
-                addDepartment([[answers.newDepartment]]);
-                console.log(
-                    chalk.yellow(`${answers.newDepartment} department added successfully`)
-                );
-                break;
-            case 'Add Role':
-                const department = await fetchDepartment("NAME", answers.newRoleDepartment);
-                addRole([[answers.newRoleTitle, answers.newRoleSalary, department[0].ID]]);
-                break;
+const aksQuestions = async () => {
+    const answers = await inquirer.prompt(questions);
+    switch (answers.action) {
+        case 'Add Department':
+            await addDepartment([[answers.newDepartment]]);
+            break;
+        case 'Add Role':
+            const department = await fetchData("department", "NAME", answers.newRoleDepartment);
+            await addRole([[answers.newRoleTitle, answers.newRoleSalary, department[0].ID]]);
+            break;
+        case 'Add Employee':
+            const role = await fetchData("role", "TITLE", answers.employeeRoleTitle);
+            const roleId = role[0].ID;
+            let managerId = null;
+            if ('None' != answers.employeeManager) {
+                const managerName = answers.employeeManager.split(" ");
+                const manager = await fetchEmployeeByName(managerName[0], managerName[1]);
+                managerId = manager[0].ID;
+            }
+            await addEmployee([[answers.employeeFirstName, answers.employeeLastName, roleId, managerId]]);
+            break;
+        case 'View Departments':
+            const departments = await fetchData("department", "1", "1");            
+            console.table("Department Summary", departments);            
+            break;
+    }
+    inquirer.prompt(continueQuestion).then(async answers => {
+        if (answers.continue) {
+            await aksQuestions();
         }
 
     });
-
 }
 initializeDisplay();
 aksQuestions();
 
-/*inquirer.prompt(continueQuestion).then(answers => {
-    if (answers.continue) {
-        aksQuestions();
-    }
 
-});*/
